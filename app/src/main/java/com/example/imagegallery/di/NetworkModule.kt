c20a00cb35a068172735bc201325d806f657fbf3
gerasymenkoke <104980001+gerasymenkoke@users.noreply.github.com>
2023-04-22 15:51:51 +0400

    Update NetworkModule.kt

--- app/src/main/java/com/example/imagegallery/di/NetworkModule.kt
+++ app/src/main/java/com/example/imagegallery/di/NetworkModule.kt
@@ -11,7 +11,7 @@ import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-private const val BASE_URL = "https://www.reddit.com/r/photos/"
+private const val BASE_URL = "https://www.reddit.com/"
 
 val networkModule = module {
 
