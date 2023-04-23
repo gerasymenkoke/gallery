64d441bcfe180742ff57519a8fe4cc42fe26ecc9
gerasymenkoke <104980001+gerasymenkoke@users.noreply.github.com>
2023-04-23 09:54:20 +0300

    Update RedditImageRemoteImpl.kt

--- app/src/main/java/com/example/imagegallery/data/network/RedditImageRemoteImpl.kt
+++ app/src/main/java/com/example/imagegallery/data/network/RedditImageRemoteImpl.kt
@@ -29,7 +29,7 @@ class RedditImageRemoteImpl(
      */
     private fun RedditPost.toImage(): RedditImage? {
         return if (postHint == IMAGE_TYPE) {
-            RedditImage(id, title, subreddit, url)
+            RedditImage(id, title, subreddit?, url)
         } else {
             null
         }
